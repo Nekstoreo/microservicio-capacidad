0821c0ab95af6e63079580694c596a6b8fb8deb4
Nestor Gutierrez <nestorg456k@outlook.com>
2026-02-13 17:06:50 -0500

    docs: add OpenAPI definition and README

---
+++ README.md
@@ -0,0 +1,23 @@
+# Microservicio Capacidad
+
+Gestión de capacidades técnicas del sistema.
+
+## Requisitos
+
+- Java 25+
+- MySQL 8.0+
+- Gradle 9.3.0+
+
+## Levantar el servicio
+
+```bash
+./gradlew bootRun
+```
+
+El servicio estará disponible en `http://localhost:8083`
+
+**OpenAPI (Swagger):** `http://localhost:8083/swagger-ui.html`
+
+## Ver más
+
+Para información del proyecto completo, consulta el [repositorio raíz](https://github.com/Nekstoreo/programacion-reactiva).
--- src/main/java/com/onclass/capacidad/CapacidadApplication.java
+++ src/main/java/com/onclass/capacidad/CapacidadApplication.java
@@ -2,7 +2,16 @@ package com.onclass.capacidad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Microservicio Capacidad",
+        version = "0.0.1",
+        description = "API para gestión de capacidades técnicas"
+    )
+)
 @SpringBootApplication
 public class CapacidadApplication {
 
