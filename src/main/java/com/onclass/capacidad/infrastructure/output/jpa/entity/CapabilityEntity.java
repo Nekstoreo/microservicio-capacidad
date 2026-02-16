d85576a7803064c9b42f71aa358416b597fac110
Nestor Gutierrez <nestorg456k@outlook.com>
2026-02-15 22:42:29 -0500

    fix: update fetch type for technologies in CapabilityEntity

--- src/main/java/com/onclass/capacidad/infrastructure/output/jpa/entity/CapabilityEntity.java
+++ src/main/java/com/onclass/capacidad/infrastructure/output/jpa/entity/CapabilityEntity.java
@@ -3,6 +3,7 @@ package com.onclass.capacidad.infrastructure.output.jpa.entity;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -26,7 +27,7 @@ public class CapabilityEntity {
     @Column(name = "description", nullable = false)
     private String description;
 
-    @OneToMany(mappedBy = "capability", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "capability", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
     private List<CapabilityTechnologyEntity> technologies = new ArrayList<>();
 
     public void addTechnology(CapabilityTechnologyEntity capabilityTechnologyEntity) {
