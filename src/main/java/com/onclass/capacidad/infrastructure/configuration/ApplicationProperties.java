987f8eb5bd65726a9a1557793a0ece079f31fb5c
Nestor Gutierrez <nestorg456k@outlook.com>
2026-02-15 23:10:15 -0500

    feat: add HTTP client credentials configuration and authorization header to WebClient

--- src/main/java/com/onclass/capacidad/infrastructure/configuration/ApplicationProperties.java
+++ src/main/java/com/onclass/capacidad/infrastructure/configuration/ApplicationProperties.java
@@ -11,6 +11,7 @@ public class ApplicationProperties {
 
     private Technology technology = new Technology();
     private Security security = new Security();
+    private HttpClient httpClient = new HttpClient();
 
     @Data
     public static class Technology {
@@ -20,5 +21,16 @@ public class ApplicationProperties {
     @Data
     public static class Security {
         private boolean enabled = true;
+    }
+
+    @Data
+    public static class HttpClient {
+        private Credentials credentials = new Credentials();
+
+        @Data
+        public static class Credentials {
+            private String username;
+            private String password;
+        }
     }
 }
